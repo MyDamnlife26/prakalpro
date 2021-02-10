528ac236e27820c6015642fb9851792ae16ed73c
MyDamnlife26 <MyDamnlife26@users.noreply.github.com>
2021-02-10 14:58:42 +0700

    1

---
+++ Latihan.py
@@ -0,0 +1,43 @@
+# h1 = 650000
+# h2 = 685000
+# h3 = 715000
+# b1 = 25
+# b2 = 15
+# t1 = (h1 * b1) + (h2 * b2)
+# t2 = (b1 + b2) * (h3)
+# profit = t2 - t1
+# print(profit)
+
+# persen_prof = (profit / t1) * 100
+# print(f"{persen_prof}%")
+
+# d1 = 200000000
+# d2 = 400000000
+# r = 10/12
+# bunga = 0.1
+# n = 1
+# bulan = 0
+# tahun = 0
+
+# while d1 < 400000000:
+#     d1 = d1 + (d1 * r/100)
+#     print("bulan ke", bulan, " ", d1)
+#     bulan = bulan + 1
+# print(f'Tahun: ', (bulan-1)/12)
+
+# while d1 <= d2:
+#     end = (d1 * bunga) + d1
+#     print(f"Tahun ke - {tahun} = Rp{end}")
+#     d1 = end
+#     tahun = tahun + 1
+
+# r = 0.1
+# n = 1
+# tahun = 0
+# A = 0
+
+# while (A <= d2):
+#     tahun = tahun + 1
+#     A = d1*(r/n)**(n*tahun)
+
+# print("Jumlah lama penyimpanan = "+str(tahun)+"tahun")
