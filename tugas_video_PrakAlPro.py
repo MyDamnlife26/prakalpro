a4ab759b21263d121960356897b69cdb011ff75a
MyDamnlife26 <MyDamnlife26@users.noreply.github.com>
2021-02-14 21:44:30 +0700

    Tugas

---
+++ tugas_video_PrakAlPro.py
@@ -0,0 +1,26 @@
+'''
+Bila sebuah katrol yang terbuat dari benda pejal dengan percepatan linear sebesar 25 m/s^2, percepatan sudut sebesar 250 rad/s^2, dan momen gaya sebesar 6 Nm. 
+Berapakah besar massa katrol?
+(Sumber Referensi = https://gurumuda.net/pembahasan-soal-un-fisika-sma-tahun-2015.htm)
+
+I = (2/5)*m*r^2
+τ = I*α
+a = r*α
+
+I = Momen inersia katrol
+m = Massa kartol
+r = Jari-jari katrol
+τ = Momen gaya
+α = Percepatan sudut
+a = Percepatan linear
+
+Ditanya = Massa katrol (m)?
+
+Input = 
+
+Proses = 
+
+Output =
+
+
+'''
