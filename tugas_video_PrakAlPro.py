b92cb875c6c7aa41efeb2580fb056dae1bd02893
MyDamnlife26 <MyDamnlife26@users.noreply.github.com>
2021-02-15 16:02:25 +0700

    update

--- tugas_video_PrakAlPro.py
+++ tugas_video_PrakAlPro.py
@@ -1,7 +1,8 @@
 '''
 Bila sebuah katrol yang terbuat dari benda pejal dengan percepatan linear sebesar 25.5 m/s^2, 
 percepatan sudut sebesar 250.5 rad/s^2, dan momen gaya sebesar 6 Nm. Berapakah besar massa 
-katrol?(Sumber Referensi = https://gurumuda.net/pembahasan-soal-un-fisika-sma-tahun-2015.htm)
+katrol (dalam satuan kg dan 1 angka di belakang koma?
+(Sumber Referensi = https://gurumuda.net/pembahasan-soal-un-fisika-sma-tahun-2015.htm)
 
 I = (2/5)*m*r^2
 τ = I*α
@@ -16,22 +17,22 @@ a = Percepatan linear
 
 Ditanya = Massa katrol (kg)?
 
-Input = a = 25.5 ; α = 250.5 ; τ = 6
+Input = a = 25.5; τ = 6 ; α = 250.5
 
 Proses = 
-1. Mencari jari-jari katrol 
-2. Mencari momen inersia
-3. Mencari massa katrol
+1. Mencari momen inersia katrol
+2. Mencari jari-jari katrol
+3. Mencari massa katrol 
 
-Output = Massa katrol (kg)
+Output = massa katrol (kg)
 
 '''
-a = float(input('Masukkan percepatan linear = '))
-b = float(input('Masukkan percepatan sudut = '))
+a = float(input('Masukkan besar percepatan linear katrol = '))
+b = float(input('Masukkan besar percepatan sudut = '))
 c = float(input('Masukkan momen gaya katrol = '))
 
-r = a/b
-I = c/b
-m = I/((2/5)*(r**2))
+I = c / b
+r = a / b
+m = I / ((2/5)*(r**2))
 
-print(f'Jadi massa katrol adalah %.1f' %(m), 'kg')
+print(f'Massa katrol adalah %.1f' %(m), 'kg')
