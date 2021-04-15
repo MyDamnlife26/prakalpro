c20fe194e8d1914ef8d59c46cafabec2c1475bb5
MyDamnlife26 <azriel.setyo@ti.ukdw.ac.id>
2021-04-15 16:29:34 +0700

    update

---
+++ Video Alpro (List).py
@@ -0,0 +1,18 @@
+'''
+Azriel Setyo Nugroho H. (71200576)
+Topik : List
+Referensi : UG
+
+
+
+'''
+cheeses = ['Cheddar', 'Edam', 'Gouda']
+numbers = [17, 123]
+empty = []
+
+print(cheeses[0])
+
+for i in range(len(numbers)):
+    numbers[i] = numbers[i] * 2
+print(numbers)
+
