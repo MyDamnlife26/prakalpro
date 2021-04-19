97dec6d0ec42bfee120d07264d1f7c4b90c664b2
MyDamnlife26 <azriel.setyo@ti.ukdw.ac.id>
2021-04-20 00:49:11 +0700

    Update Video Alpro (List).py

--- Video Alpro (List).py
+++ Video Alpro (List).py
@@ -2,17 +2,25 @@
 Azriel Setyo Nugroho H. (71200576)
 Topik : List
 Referensi : UG
+Bambang membuka sebuah rumah makan, dia ingin membuat menu yang berbeda setiap harinya.
+Buatlah sebuah program sederhana untuk membuat menu rumah makan milik bambang. 
 
+input:
+1. Nama dan harga menu di rumah makan
+2. Pilihan menu dalam program (menambahkan isi daftar menu, menghapus isi daftar menu, menampilkan isi menu sekaligus keluar dari program)
 
+proses:
+1. Membuat percabangan untuk pilihan pengguna
+2. Membuat perulangan untuk menambahkan ini menu
+3. Membuat percabangan untuk menghapus poin khusus dari menu atau secara keseluruhan
+4. membuat perulangan untuk menampilkan isi menu
+
+output:
+========== Menu Rumah Makan Bambang ==========
+1. Ayam           Rp. 10.000,00
+2. Telur          Rp. 10.000,00
+3. Rendang        Rp. 15.000,00
+4. Sayur          Rp. 8.000,00
 
 '''
-cheeses = ['Cheddar', 'Edam', 'Gouda']
-numbers = [17, 123]
-empty = []
-
-print(cheeses[0])
-
-for i in range(len(numbers)):
-    numbers[i] = numbers[i] * 2
-print(numbers)
 
