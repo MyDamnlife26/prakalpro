8e226f27f9d666097e4abc87884adaa6fd02c2d6
MyDamnlife26 <azriel.setyo@ti.ukdw.ac.id>
2021-04-25 11:01:28 +0700

    update

--- Video Alpro (List).py
+++ Video Alpro (List).py
@@ -3,7 +3,7 @@ Azriel Setyo Nugroho H. (71200576)
 Topik : List
 Referensi : UG
 Bambang membuka sebuah rumah makan, dia ingin membuat menu yang berbeda setiap harinya.
-Buatlah sebuah program sederhana untuk membuat menu rumah makan milik bambang. 
+Buatlah sebuah program sederhana untuk membuat menu rumah makan milik Bambang. 
 
 input:
 1. Nama dan harga menu di rumah makan
@@ -23,52 +23,52 @@ output:
 4. Sayur          Rp. 8.000,00
 
 '''
-
 nama = []
 harga = []
 
 while True:
-    print("""--------------------------------- SELAMAT DATANG ---------------------------------
-SILAHKAN PILIH MENU DI BAWAH:
-1. Tambah menu
-2. Hapus menu
-3. Lihat menu
-4. Keluar""")
+    print("-------------------------- SELAMAT DATANG --------------------------")
+    print("""Silahkan pilih menu dibawah ini:
+    1. Tambah Menu
+    2. Hapus Menu
+    3. Lihat Menu
+    4. Keluar""")
     masuk = int(input("Masukkan pilihan: "))
     if masuk == 1:
-        masuk1 = int(input("Jumlah menu yang akan dimasukkan: "))
+        masuk1 = int(input("Jumlah menu yang akan ditambahkan: "))
         for i in range(masuk1):
             name = input("Masukkan nama menu: ")
-            nama.append(name.title())
             price = input("Masukkan harga menu: ")
+            nama.append(name.title())
             harga.append(price)
             print("*"*50)
     elif masuk == 2:
-        print("""Pilihan: 
-1. Hapus poin Khusus
+        print("""Pilihan:
+1. Hapus poin khusus
 2. Hapus keseluruhan""")
         masuk1 = int(input("Masukkan pilihan: "))
         if masuk1 == 1:
-            masuk2 = input("Masukkan nama menu: ")
+            masuk2 = input("Masukkan nama menu yang akan dihapus: ")
             masuk2 = masuk2.title()
             if masuk2 in nama:
                 indexMenu = nama.index(masuk2)
                 nama.remove(masuk2)
                 harga.remove(harga[indexMenu])
-                print(f"{masuk2} berhasil dihapus dari menu.")
             else:
-                print("Input tidak ditemukan dalam menu.")
-        if masuk1 == 2:
-            nama.clear()
+                print("Input non valid")
+        elif masuk1 == 2:
             harga.clear()
+            nama.clear()
+        else:
+            print("Input non valid")
     elif masuk == 3:
-        c = 1
         print("========== Menu Rumah Makan Bambang ==========")
+        c = 1
         for i in range(len(nama)):
-            print(f"{c}.", nama[i], "    ", "\t", f"Rp. {harga[i]},00")
+            print(f"{c}.", nama[i], "    ", " \t\t", f"Rp. {harga[i]},00")
             c += 1
     elif masuk == 4:
-        print("--------------------------------- SELAMAT TINGGAL ---------------------------------")
+        print(("-------------------------- SELAMAT TINGGAL --------------------------"))
         break
     else:
-        print("Input error.")
+        print("Input non valid")
