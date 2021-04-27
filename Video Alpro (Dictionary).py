a6d5c723ccbb1ad3f98165f191544b6ed1317e2e
MyDamnlife26 <azriel.setyo@ti.ukdw.ac.id>
2021-04-27 10:54:42 +0700

    Create Video Alpro (Dictionary).py

---
+++ Video Alpro (Dictionary).py
@@ -0,0 +1,29 @@
+'''
+Azriel Setyo Nugroho H. (71200576)
+Topik : Dictionary
+Referensi : UG
+Pak Sebas adalah pekerja dari bagian tata usaha sekolah hunter.
+Dia merasa bingung harus bagaimana supaya dapat mendata data pengecekan kesehatan di sekolahnya.
+Bantulah Pak Sebas membuat sebuah sistem untuk mendata murid yang ada di sekolahnya.
+
+input:
+1. Pilihan pengguna dalam mengakses sistem
+2. Nama siswa sebagai key
+3. Kelas, berat, dan tinggi siswa sebagai value
+
+proses:
+1. Buat perulangan untuk sistem
+2. Buat percabangan untuk pilihan user
+3. Buat inputan untuk memasukkan data
+4. Buat perulangan untuk melihat data
+5. Buat perulangan dan percabangan untuk menghapus data
+6. Buat pilihan untuk keluar dari sistem
+
+output:
+------------------------- Data Siswa -------------------------
+1.      Nama: Andi      Kelas: 2        Berat: 80       Tinggi: 190
+2.      Nama: Lala      Kelas: 2        Berat: 68       Tinggi: 178
+3.      Nama: Jai       Kelas: 3        Berat: 80       Tinggi: 189
+4.      Nama: Rindu     Kelas: 3        Berat: 67       Tinggi: 169
+
+'''
