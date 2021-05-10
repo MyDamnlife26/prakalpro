d50766be5bc3d8ca03a274997cf0c771dd1af88f
MyDamnlife26 <azriel.setyo@ti.ukdw.ac.id>
2021-05-11 00:17:17 +0700

    my

---
+++ Video Alpro (Sets).py
@@ -0,0 +1,26 @@
+'''
+Azriel Setyo Nugroho H. (71200576)
+Topik : Set
+Referensi : UG
+Pak Sebas adalah pekerja dari bagian tata usaha sekolah hunter.
+Dia merasa bingung harus bagaimana supaya dapat mendata data pengecekan kesehatan di sekolahnya.
+Bantulah Pak Sebas membuat sebuah sistem untuk mendata murid yang ada di sekolahnya.
+
+input:
+1. Pilihan pengguna dalam mengakses sistem (input data, lihat data, hapus data, dan keluar dari sistem)
+2. Nama siswa sebagai key
+3. Kelas, berat, dan tinggi siswa sebagai value
+
+proses:
+1. Buat perulangan untuk sistem
+2. Buat percabangan untuk pilihan user
+3. Buat inputan untuk memasukkan data
+4. Buat perulangan untuk melihat data
+5. Buat perulangan dan percabangan untuk menghapus data
+6. Buat pilihan untuk keluar dari sistem
+
+output:
+
+
+'''
+
