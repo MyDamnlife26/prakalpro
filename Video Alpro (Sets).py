2266274e2a1934c63de3c8c606bc8625243197f7
MyDamnlife26 <azriel.setyo@ti.ukdw.ac.id>
2021-05-11 10:02:01 +0700

    Update Video Alpro (Sets).py

--- Video Alpro (Sets).py
+++ Video Alpro (Sets).py
@@ -2,9 +2,9 @@
 Azriel Setyo Nugroho H. (71200576)
 Topik : Set
 Referensi : UG
-Pak Sebas adalah pekerja dari bagian tata usaha sekolah hunter.
-Dia merasa bingung harus bagaimana supaya dapat mendata data pengecekan kesehatan di sekolahnya.
-Bantulah Pak Sebas membuat sebuah sistem untuk mendata murid yang ada di sekolahnya.
+Rumongso adalah seorang guru sekolah dasar di SD Suka Maju Mundur
+Dia ingin mendata semua siswa yang ada di kelasnya, namun takut apabila memasukkan data lebih dari satu kali
+Buatkan lah program untuk mendata siswa di SD tempat mengajar Pak Rumongso
 
 input:
 1. Pilihan pengguna dalam mengakses sistem (input data, lihat data, hapus data, dan keluar dari sistem)
@@ -12,15 +12,45 @@ input:
 3. Kelas, berat, dan tinggi siswa sebagai value
 
 proses:
-1. Buat perulangan untuk sistem
-2. Buat percabangan untuk pilihan user
-3. Buat inputan untuk memasukkan data
-4. Buat perulangan untuk melihat data
-5. Buat perulangan dan percabangan untuk menghapus data
-6. Buat pilihan untuk keluar dari sistem
+1. 
 
 output:
+Silahkan pilih hal yang Anda inginkan:
+1. Menambahkan data siswa
+2. Menghapus data siswa
+3. Menampilkan data siswa
+4. Keluar
+Masukkan pilihan:
 
 
+Berapa jumlah siswa kelas A yang ingin dimasukkan?
+Masukkan angka: 3
+Silahkan masukkan nama siswa: ju
+Silahkan masukkan nama siswa: ki
+Silahkan masukkan nama siswa: nan
+
+
+Siswa kelas mana yang ingin Anda hapus (A/B)?
+Masukkan pilihan Anda: b
+
+Berapa jumlah siswa yang ingin Anda hapus?
+Masukkan angka: 1
+Masukkan nama siswa: ka
+
+
+-------------------------------- KELAS A --------------------------------
+1. Di
+2. Ju
+3. Ki
+4. Nan
+
+-------------------------------- KELAS B --------------------------------
+1. An
+2. Cur
+3. Da
+4. Han
+5. Ji
+
+Jumlah siswa saat ini adalah 9
 '''
 
